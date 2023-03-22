9a01ae5fbdf7b2cc1acab20fb38bf9fc8b33d12c
Coralie2798 <coralie.bodiguel@gmail.com>
2023-03-22 11:57:35 +0100

    classe Destination

---
+++ src/main/java/com/inti/Destination.java
@@ -0,0 +1,37 @@
+package com.inti;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode.Exclude;
+import lombok.NoArgsConstructor;
+
+@Entity @Table
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Destination {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private long longitude;
+	private long latitude;
+//	
+//	@Exclude
+//	@OneToMany @JoinColumn(name="id_hotel")
+//	private List<Hotel> listeHotel;
+	
+	public Destination(long longitude, long latitude) {
+		super();
+		this.longitude = longitude;
+		this.latitude = latitude;
+	}
+	
+	
+}
