f78e727700123a623c8e77cfd24932108c83294d
libel <libel@LAPTOP-P1GR4IVV>
2023-03-22 12:06:33 +0100

    Merge branch 'master' of https://github.com/Jaolerr/TP1Jenkins.git

--- src/main/java/com/inti/model/Hotel.java
+++ src/main/java/com/inti/model/Hotel.java
@@ -1,13 +1,17 @@
 package com.inti.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Entity @Data
 @NoArgsConstructor @AllArgsConstructor
@@ -18,6 +22,12 @@ public class Hotel {
 	private long idHotel;
 	private String nom;
 	private int nbEtoile;
+
+	@ManyToOne
+	@JoinColumn(name="id")
+	private Destination destination;
+	@OneToMany @JoinColumn(name="idReserv")
+	private List<Reservation> reservation;
 	
 	public Hotel(String nom, int nbEtoile) {
 		super();
