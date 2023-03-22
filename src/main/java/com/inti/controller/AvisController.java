48cb854abded4f5194a22757fe5d1308a6532c15
InfoJavaF <florian.belin@itech.fr>
2023-03-22 12:24:38 +0100

    Create avis

--- src/main/java/com/inti/controller/AvisController.java
+++ src/main/java/com/inti/controller/AvisController.java
@@ -3,17 +3,28 @@ package com.inti.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.inti.model.Avis;
 import com.inti.repository.IAvisRepository;
 
 @Controller
 public class AvisController {
 
 	@Autowired
-	IAvisRepository iaR;
+	IAvisRepository iar;
 	
 	@GetMapping("createAvis")
 	public String createAvisForm() {
 		return "createAvisForm";
 	}
+	
+	@PostMapping("createAvis")
+	public String createAvis(@ModelAttribute("avis") Avis a) {
+		iar.save(a);
+		return "redirect:/createAvis";
+	}
+	
 }
