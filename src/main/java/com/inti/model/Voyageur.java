85fbc08a1c81be74de1bd41931b27efb4f772298
jeremy <jeremy.claude4@gmail.com>
2023-03-22 12:20:26 +0100

    Repo + fix

--- src/main/java/com/inti/model/Voyageur.java
+++ src/main/java/com/inti/model/Voyageur.java
@@ -3,6 +3,9 @@ package com.inti.model;
 import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -11,6 +14,8 @@ import lombok.Data;
 @Table
 @Data
 public class Voyageur {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nom;
 	private String prenom;
